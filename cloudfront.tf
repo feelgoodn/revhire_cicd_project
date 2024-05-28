790d525d2f4a4b8e1391f37b1ec45ff303a72b0e
feelgoodn <nikhilkotale2000iit@gmail.com>
2024-05-28 10:08:02 +0530

    updated files

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,76 +1,76 @@
 
-# resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
-#   comment = "Origin access identity for static website"
-# }
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Origin access identity for static website"
+}
 
 
-# resource "aws_cloudfront_distribution" "cloudfront_distribution" {
-#   origin {
-#     domain_name = aws_s3_bucket.mybucket.bucket_regional_domain_name
-#     origin_id   = aws_s3_bucket.mybucket.id
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.mybucket.bucket_regional_domain_name
+    origin_id   = aws_s3_bucket.mybucket.id
 
-#     s3_origin_config {
-#       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
-#     }
-#   }
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
 
-#   enabled             = true
-#   default_root_object = var.website_index_document
+  enabled             = true
+  default_root_object = var.website_index_document
 
-#   default_cache_behavior {
-#     allowed_methods  = ["GET", "HEAD"]
-#     cached_methods   = ["GET", "HEAD"]
-#     target_origin_id = aws_s3_bucket.mybucket.id
-
-
-#     forwarded_values {
-#       query_string = false
-
-#       cookies {
-#         forward = "none"
-#       }
-#     }
-
-#     viewer_protocol_policy = "redirect-to-https"
-#     min_ttl                = 0
-#     default_ttl            = 3600
-#     max_ttl                = 86400
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#   }
-
-#   restrictions {
-#     geo_restriction {
-#       restriction_type = "none"
-#     }
-#   }
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.mybucket.id
 
 
-#   tags = {
-#     Name        = "Revhire Cloudfront"
-#     Environment = "Dev"
+    forwarded_values {
+      query_string = false
 
-#   }
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 
 
-# }
+  tags = {
+    Name        = "Revhire Cloudfront"
+    Environment = "Dev"
 
-# resource "aws_s3_bucket_policy" "website_bucket_policy" {
-#   bucket = aws_s3_bucket.mybucket.id
+  }
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:GetObject"
-#         Resource  = "${aws_s3_bucket.mybucket.arn}/*"
 
-#       }
-#     ]
-#   })
-# }
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.mybucket.arn}/*"
+
+      }
+    ]
+  })
+}
 
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-# output "s3_bucket_name" {
-#   value = aws_s3_bucket.mybucket.id
+output "s3_bucket_name" {
+  value = aws_s3_bucket.mybucket.id
 
-# }
+}
 
-# output "cloudfront_distribution_domain_name" {
-#   value = aws_cloudfront_distribution.cloudfront_distribution.domain_name
+output "cloudfront_distribution_domain_name" {
+  value = aws_cloudfront_distribution.cloudfront_distribution.domain_name
 
-# }
+}
